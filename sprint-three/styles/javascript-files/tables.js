e36465572fec84762fb3933bde83b6111fcd6806
Melissa Lima Lopes <melissalimalopes@Julias-iPhone.localdomain>
2019-10-21 15:49:33 -0700

    create sprint three

---
+++ sprint-three/styles/javascript-files/tables.js
@@ -0,0 +1,82 @@
+let show = axios.get(
+  "https://project-1-api.herokuapp.com/comments?api_key=melissa"
+);
+
+// const showInfo = [
+//   {
+//     date: "Dec 17 2018",
+//     venue: "Ronald Lane",
+//     location: "San Fancisco, CA"
+//   },
+//   {
+//     date: "Jul 18 2019",
+//     venue: "Pier 3 East",
+//     location: "San Fancisco, CA"
+//   },
+//   {
+//     date: "Jul 22 2019",
+//     venue: "View Loungue",
+//     location: "San Fancisco, CA"
+//   },
+//   {
+//     date: "Aug 12 2019",
+//     venue: "Hyatt Agency",
+//     location: "San Fancisco, CA"
+//   },
+//   {
+//     date: "Sep 05 2019",
+//     venue: "Pres Club",
+//     location: "San Fancisco, CA"
+//   }
+// ];
+
+function tableMobile(table, showInfo) {
+  let rowTop = table.insertRow();
+  rowTop.classList.add("shows__tablet-top");
+  let cellTop = rowTop.insertCell();
+  let textTop = document.createTextNode("DATE");
+  cellTop.appendChild(textTop);
+  cellTop.classList.add("shows__tablet-head-date");
+
+  let cellTopSec = rowTop.insertCell();
+  let textTopSec = document.createTextNode("VENUE");
+  cellTopSec.appendChild(textTopSec);
+  cellTopSec.classList.add("shows__tablet-head-venue");
+
+  let cellTopThird = rowTop.insertCell();
+  let textTopThird = document.createTextNode("LOCATION");
+  cellTopThird.appendChild(textTopThird);
+  cellTopThird.classList.add("shows__tablet-head-location");
+
+  for (show of showInfo) {
+    let row = table.insertRow();
+    row.classList.add("shows-table");
+
+    for (key in show) {
+      let cell = row.insertCell();
+      cell.classList.add("shows__mobile-table-title");
+      let cellTwo = row.insertCell();
+      cellTwo.classList.add("shows__mobile-table-value");
+      let text = document.createTextNode(key);
+      let textTwo = document.createTextNode(show[key]);
+      cell.appendChild(text);
+      cellTwo.appendChild(textTwo);
+    }
+
+    let cell = row.insertCell();
+    cell.classList.add("shows__mobile-button");
+    let text = document.createTextNode("BUY TICKETS");
+    cell.appendChild(text);
+    // let rowTwo = table.insertRow();
+    // let cell = rowTwo.insertCell();
+    // cell.classList.add("shows__mobile-button");
+    // let text = document.createTextNode("BUY TICKETS");
+    // cell.appendChild(text);
+  }
+}
+
+let table = document.querySelector(".table-mobile");
+
+//let showKeys = Object.keys(showInfo[0]);
+// function invocation
+tableMobile(table, showInfo);
--- sprint-two/styles/javascript-files/comments.js
+++ sprint-two/styles/javascript-files/comments.js
@@ -1,24 +1,3 @@
-let commentsArray = [
-  {
-    name: "Michael Lyons",
-    comment:
-      "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
-    date: "12/18/2018"
-  },
-  {
-    name: "Gary Wong",
-    comment:
-      "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
-    date: "12/12/2018"
-  },
-  {
-    name: "Theodore Duncan",
-    comment:
-      "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
-    date: "11/15/2018"
-  }
-];
-
 function createComment(commentsArray) {
   const commentSection = document.querySelector(".comments__row");
 
@@ -26,6 +5,7 @@ function createComment(commentsArray) {
     let commentOne = document.createElement("div");
     commentOne.classList.add("comments__row-class");
     commentSection.appendChild(commentOne);
+    console.log("check", commentsArray[i]);
 
     let button = document.createElement("button");
     commentOne.appendChild(button);
@@ -38,7 +18,7 @@ function createComment(commentsArray) {
 
     let dateTitle = document.createElement("h3");
     commentOne.appendChild(dateTitle);
-    dateTitle.innerText = commentsArray[i].date;
+    dateTitle.innerText = commentsArray[i].timestamp;
     dateTitle.classList.add("comments__row-date");
 
     let commentElement = document.createElement("p");
@@ -48,7 +28,70 @@ function createComment(commentsArray) {
   }
 }
 
-createComment(commentsArray);
+let com = axios
+  .get("https://project-1-api.herokuapp.com/comments?api_key=melissa")
+  .then(response => {
+    console.log(response.data);
+
+    return response.data;
+  })
+
+  .then(response => {
+    console.log("response", response);
+    createComment(response);
+  });
+
+// let commentsArray = [
+//   {
+//     name: "Michael Lyons",
+//     comment:
+//       "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
+//     date: "12/18/2018"
+//   },
+//   {
+//     name: "Gary Wong",
+//     comment:
+//       "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
+//     date: "12/12/2018"
+//   },
+//   {
+//     name: "Theodore Duncan",
+//     comment:
+//       "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
+//     date: "11/15/2018"
+//   }
+// ];
+
+// createComment(commentsArray);
+
+// function createComment(commentsArray) {
+//   const commentSection = document.querySelector(".comments__row");
+
+//   for (let i = 0; i < com.length; i++) {
+//     let commentOne = document.createElement("div");
+//     commentOne.classList.add("comments__row-class");
+//     commentSection.appendChild(commentOne);
+
+//     let button = document.createElement("button");
+//     commentOne.appendChild(button);
+//     button.classList.add("comments__row-button");
+
+//     let nameTitle = document.createElement("h2");
+//     commentOne.appendChild(nameTitle);
+//     nameTitle.innerText = commentsArray[i].name;
+//     nameTitle.classList.add("comments__row-name");
+
+//     let dateTitle = document.createElement("h3");
+//     commentOne.appendChild(dateTitle);
+//     dateTitle.innerText = commentsArray[i].date;
+//     dateTitle.classList.add("comments__row-date");
+
+//     let commentElement = document.createElement("p");
+//     commentOne.appendChild(commentElement);
+//     commentElement.innerText = com[i].comment;
+//     commentElement.classList.add("comments__row-comment");
+//   }
+// }
 
 let form = document.querySelector(".comments");
 
